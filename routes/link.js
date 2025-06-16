0876d06f7f338eb628a769b7b3e67a5f01b1ba19
Umang Ghatbandhe <umangghatbandhe0@gmail.com>
2025-06-16 20:10:25 +0530

    d

--- routes/link.js
+++ routes/link.js
@@ -3,6 +3,7 @@ import fetch from "node-fetch"; // Install if not already: npm i node-fetch
 const router = express.Router();
 
 router.post("/api/validate-link", async (req, res) => {
+  console.log("ImInside this api!!!!!!!!!!!");
   const { url } = req.body;
 
   if (!url || typeof url !== "string") {
